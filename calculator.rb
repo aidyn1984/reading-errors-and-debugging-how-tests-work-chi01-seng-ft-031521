81dfe08b50ef80de1e3a0fb6b16282213c67a476
Aiden Seitz <csa.aiden@gmail.com>
2021-02-20 02:47:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,20 +1,5 @@
 # Add your variables here
-def sum(1, 2)
+sum(first_number, second_number)
   sum = 1 + 2
   sum
-end
-
-def difference(1, 2)
-  difference = 1 - 2
-  difference
-end
-
-def product(1, 2)
-  product = 1 * 2
-  product
-end
-
-def quotient(1, 2)
-  quotient = 1 / 2
-  quotient
 end
