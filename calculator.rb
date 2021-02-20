fb4ee7b824e320c8b423af1ef6d1cbfd0ab9d76e
Aiden Seitz <csa.aiden@gmail.com>
2021-02-20 02:46:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,47 +1,20 @@
 # Add your variables here
-def sum(first_number, second_number)
-  sum = first_number + second_number
+def sum(1, 2)
+  sum = 1 + 2
   sum
 end
 
-def difference(first_number, second_number)
-  difference = first_number - second_number
+def difference(1, 2)
+  difference = 1 - 2
   difference
 end
 
-def product(first_number, second_number)
-  product = first_number * second_number
+def product(1, 2)
+  product = 1 * 2
   product
 end
 
-def quotient(first_number, second_number)
-  quotient = first_number / second_number
+def quotient(1, 2)
+  quotient = 1 / 2
   quotient
 end
-
-
-# d
-#   it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
-#     first_number = get_variable_from_file('./calculator.rb', "first_number")
-#     second_number = get_variable_from_file('./calculator.rb', "second_number")
-#     difference = get_variable_from_file('./calculator.rb', "difference")
-
-#     expect(difference).to eq(first_number-second_number)
-#   end
-
-#   it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
-#     first_number = get_variable_from_file('./calculator.rb', "first_number")
-#     second_number = get_variable_from_file('./calculator.rb', "second_number")
-#     product = get_variable_from_file('./calculator.rb', "product")
-
-#     expect(product).to eq(first_number*second_number)
-#   end
-
-#   it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
-#     first_number = get_variable_from_file('./calculator.rb', "first_number")
-#     second_number = get_variable_from_file('./calculator.rb', "second_number")
-#     quotient = get_variable_from_file('./calculator.rb', "quotient")
-
-#     expect(quotient).to eq(first_number/second_number)
-#   end
-# end
