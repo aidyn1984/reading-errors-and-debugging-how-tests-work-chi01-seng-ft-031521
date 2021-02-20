25db69ccc18a9b08e386e787f10869ad15b6708b
Aiden Seitz <csa.aiden@gmail.com>
2021-02-20 02:54:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,10 @@
 # Add your variables here
 def sum(first_number, second_number) 
-first_number = 1
-second_number = 2
-sum = first_number + second_number
-sum
+  first_number = 1
+  second_number = 2
+  sum = first_number + second_number
+  sum
+end
 
 
 # describe "./calculator.rb" do
